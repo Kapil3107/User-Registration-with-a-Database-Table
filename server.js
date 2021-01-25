76123a59c93460902493339aef42d657a0c27c0b
Kapil3107 <51228219+Kapil3107@users.noreply.github.com>
2021-01-25 22:35:16 +0530

    deploying

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTo
 // routes
 app.use('/api/infos', require('./routes/api/infos'))
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
