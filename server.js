c52f3446403a7195a77c9a397ac8592e84162f48
Kapil3107 <51228219+Kapil3107@users.noreply.github.com>
2021-01-29 17:15:40 +0530

    changed

--- server.js
+++ server.js
@@ -16,8 +16,7 @@ mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTo
 app.use('/api/infos', require('./routes/api/infos'))
 
 if (process.env.NODE_ENV === 'production') {
-    //app.use(express.static('client/build'));
-    app.use('/static', express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
