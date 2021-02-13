fd9d10076e897c8151391c825c0f908a1037f6ca
Kapil3107 <51228219+Kapil3107@users.noreply.github.com>
2021-02-13 21:40:11 +0530

    changed

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
-import Users from './Users';
-import Submit from './Submit';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import App from "./App";
+import Users from "./Users";
+import Submit from "./Submit";
+import reportWebVitals from "./reportWebVitals";
 
-import { Provider } from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux";
+import store from "./store";
 
 // Font awesome
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faSort, faAngleDoubleLeft, faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import {
+  faCheckSquare,
+  faSort,
+  faAngleDoubleLeft,
+  faAngleDoubleRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 library.add(fab, faCheckSquare, faSort, faAngleDoubleLeft, faAngleDoubleRight);
 
@@ -20,13 +25,15 @@ ReactDOM.render(
   <Provider store={store}>
     <Router>
       <div>
-        <Route exact path="/" component={App} />
-        <Route path="/users" component={Users} />
-        <Route path="/submit" component={Submit} />
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/users" component={Users} />
+          <Route path="/submit" component={Submit} />
+        </Switch>
       </div>
     </Router>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
